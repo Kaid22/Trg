5db32f5728f0daf326c0ac5812b24cefb8636c99
Kaid22 <avdickaid@gmail.com>
2020-12-08 13:13:09 +0100

    komita

--- trg/Form1.cs
+++ trg/Form1.cs
@@ -16,5 +16,18 @@ namespace trg
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int x1 = Convert.ToInt32(textBox1.Text);
+            int x2 = Convert.ToInt32(textBox2.Text);
+            int y1 = Convert.ToInt32(textBox3.Text);
+            int y2 = Convert.ToInt32(textBox4.Text);
+            float duzina = (x2 - x1) * 2 + (y2 - y1)*2;
+            float povrsina = (x2 - x1) * (y2 - y1);
+            label7.Text = povrsina.ToString();
+            label8.Text = duzina.ToString();
+        }
+
     }
 }
